45b17bbdd8c9fa264cdd3276db7bc7ed751c079b
Jacob-D-000 <w0478482@nscc.ca>
2022-10-26 08:42:54 -0300

    Bigger pain

--- Math_Operators.py
+++ Math_Operators.py
@@ -18,13 +18,28 @@ minus_result = float(num2) - float(num1)
 print("The result of subtracting the 2nd Number form the 1st is %s." % minus_result)
 
 # Negation Demo
+neg_result = - int(num1)
+print(f"The result of negating your first number is {neg_result}. ")
+
+neg_result = - int(num2)
+print(f"The result of negating your second number is {neg_result}. ")
 
 # Exponentiation demo
+ex_result = int(num1) ** int(num2)
+print(f"The result of multiplying the first number by the second number exponentially is {ex_result}")
 
 # Multiplication Demo
+multi_result = int(num1) * int(num2)
+print(f"The result of multiplying the first number by the second number is {multi_result}.")
 
 # Division Demo
+div_result = int(num2) / int(num1)
+print(f"The result of dividing the second number by the first is {div_result}")
 
 # Quotient Demo
+quo_result = int(num2) // int(num1)
+print(f"The resulting floor of dividing the first number form the second is {quo_result}")
 
 # Modulus Demo
+mod_result = int(num2) % int(num1)
+print(f'The resulting modulus (or remainder) from dividing the first number by the seconded number is {mod_result}')
