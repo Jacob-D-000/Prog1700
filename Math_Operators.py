f01396f3bb35c551b479e3228be7093c3edfe4cc
Jacob-D-000 <w0478482@nscc.ca>
2022-10-26 08:35:19 -0300

    This is a test

---
+++ Math_Operators.py
@@ -0,0 +1,30 @@
+"""
+Program Math Operators
+Author: Jacob Dimoff
+Date 24/10/22
+Filename:Math_Operators.py
+Purpose: get two numbers as input and demo the various math operators
+"""
+
+num1 = input("Enter 1st Number: ")
+num2 = input("Enter 2nd Number(Larger than First): ")
+
+# Addition Demo
+sum_result = int(num1) + int(num2)
+print("The sum of the two numbers is {}. ".format(sum_result))
+
+# Subtraction Demo
+minus_result = float(num2) - float(num1)
+print("The result of subtracting the 2nd Number form the 1st is %s." % minus_result)
+
+# Negation Demo
+
+# Exponentiation demo
+
+# Multiplication Demo
+
+# Division Demo
+
+# Quotient Demo
+
+# Modulus Demo
