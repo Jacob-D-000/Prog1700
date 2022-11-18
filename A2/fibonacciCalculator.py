e66c570229b958bd51398f55eb00c3145b2d1711
Jacob-D-000 <w0478482@nscc.ca>
2022-11-18 09:14:48 -0400

    Modified before the due date

--- A2/fibonacciCalculator.py
+++ A2/fibonacciCalculator.py
@@ -8,24 +8,33 @@ Purpose: To calculate the Fibonacci sequence in a range of 1 to 100
 
 import time
 
-input("This program will print the Fibonacci Numbers from 1 to 100. Press enter to run the Program: ")
+# input that lets user start the function when they press enter
+#input("This program will print the Fibonacci Numbers from 1 to 100. Press enter to run the Program: ")
 
 # variable one is
 x = 0
-
+# Including a print for x as it is the first number in the sequence
+print(x)
 # variable two is
 y = 1
-
+# Including a print for y as it is the second number in the sequence
+print(y)
 # Defining the varible that will be printed
-
 z = x + y
-while x < 100 and y < 100:
-    if z < 100:
-        print(z)
-        z = x + y
-        x = y
-        y = z
-        time.sleep(1)
+
+
+# begining of the while loop. This statement sets the loop to not go over 100
+while z < 100:
+    # this prints the first Z as well as well as well as the changed z in preiovus cycles of the loop
+    print(z)
+    # equation changes variable 
+    z = x + y
+    # changing the varible of x to be the same value as y for the next cycle
+    x = y
+    # changing the varible of y to be the same value as z for the next cycle
+    y = z
+    # time fuction for added flair
+    time.sleep(1)
 
 
 print("All done")
