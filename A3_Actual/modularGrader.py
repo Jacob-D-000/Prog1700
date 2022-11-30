dab0a3d797ac5963f859139c940897fe89f6ce3f
Jacob-D-000 <w0478482@nscc.ca>
2022-11-30 16:26:15 -0400

    Finally Done!

--- A3_Actual/modularGrader.py
+++ A3_Actual/modularGrader.py
@@ -6,18 +6,6 @@ filename: modularGrader.py
 Purpose: Practice modularization of fuctions by writing a program to calculate grades
 """
 
-# def displayInstructions():
-
-# def validateGrade(x):
-#     if x == A or a:
-        
-#     elif x == B or b:
-
-#     elif x == C or c:
-
-#     elif x == D or d:
-#     elif x == F or f:          
-
 def instructions():
     print("This Fuction can calculate an average grade out of 100%. This average could be for an an entire class or for a single indivdual.\nThis program will not accpet letter grades or equations. \n\nIf you want to finsih entering grades, type and enter exit. \nIf you need to see the rules of this fuction again, type -?.")
 
@@ -29,34 +17,48 @@ def classorperson():
             cvp = input("Can't compute! Please enter 1 to calculate the class average, or 2 to calculate the indivdual average. \n\nPlease Enter Here: ")
         else:
             convar += 1 
-        # :
-        #     cvp = input("Can't compute! Please enter 1 to calculate the class average, or 2 to calculate the indivdual average. \n\nPlease Enter Here: ") 
-        # elif cvp == "2":
     return cvp
+
+def validateGrade(x):
+    if x == "-?":
+        print("This program will not accpet letter grades or equations. If you want to finsih entering grades, type and enter exit. If you \nneed to see the rules of this fuction again, type -?.")
+        var = False
+    elif x == "exit":
+        print("Thanks for the input")
+        var = False
+    elif x.isalpha():
+        print("Won't compute. Please input a grade. If you need help remebering the fuction's restrictions please enter -?.")
+    elif x.isalpha() and x.isnumeric():
+        print("Won't compute. Please input a grade. If you need help remebering the fuction's restrictions please enter -?.")
+        var = False
+    elif "." in x or x.isnumeric():
+        var = True
+    else:
+       print("Won't compute. Please input a grade. If you need help remebering the fuction's restrictions please enter -?.")
+       var = False 
+    return var
+
+def rancheck(x):
+    if 0 < x < 100:
+        var = True
+    else:
+        var = False
+        print("Number is not in range of 0 - 100. Please input a number in the proper range")
+    return var
 
 def getGrades():
     grdlst = []
-    grd = 0
-    # oplst = ["+","-" ,"*" ,"/", "."]
-    while grd != -1:
-        grd = (input("Please input a grade: "))
-        bf = grd.() 
-        bi = bool(int(grd))
-        if grd == "-?":
-            print( "This program will not accpet letter grades or equations. If you want to finsih entering grades, type and enter exit. If you \nneed to see the rules of this fuction again, type -?.")
-            # grd = input("Please input a grade: ")
-        # elif grd != float(grd) or grd != int(grd):
-        elif False == bf and  False == bi:
-            print("Won't compute. Please input a grade. If you need help remebering the fuction's restrictions please enter -?.")
-        # for grd in grd.split():
-            # if grd in ["+","-" ,"*" ,"/", "."] or grd.isnumeric():
-            #     neogrd = eval(grd)
-            #     grdlst.append(neogrd)
-            # elif grd.isalpha():
-            #     grd
-        elif True == bf or True == bi:
+    grd = None
+    while grd != "exit":
+        grd = input("Input a Grade: ")
+        ggbool = (validateGrade(grd))
+        if ggbool:
             grd = float(grd)
-            grdlst.append(grd)
+            neobool = rancheck(grd)
+            if neobool:
+                grdlst.append(grd)
+        
+
     return grdlst
 
 def calculateAvg(x):
@@ -73,13 +75,26 @@ def resultype(x):
 def displayResults(x):
     print(x)
 
-def main():
-    instructions()
-    print("\n\n")
-    cvp = classorperson()
-    grdlst = getGrades()
-    grdavg = calculateAvg(grdlst)
-    resultype(cvp)
-    displayResults(grdavg)
+def convarloop():
+    yorn = input("Thanks for using. \n\nIf you would like to find another average, type yes. \n\nIf you would like to exit the program, type no \n\nEnter command here: ")
+    if yorn == "yes":
+        convar = 1
+    elif yorn == "no":
+        convar = 0
+    else: 
+       yorn = input("Won't compute! \n\nIf you would like to find another average, type yes. \n\nIf you would like to exit the program, type no \n\nEnter command here: ")
+    return convar
 
+def main():
+    convar = 1
+    instructions()
+    while convar == 1:
+        print("\n\n")
+        cvp = classorperson()
+        grdlst = getGrades()
+        grdavg = calculateAvg(grdlst)
+        resultype(cvp)
+        displayResults(grdavg)
+        convar = convarloop()
+    print("Thanks for using the Program!")
 main()
